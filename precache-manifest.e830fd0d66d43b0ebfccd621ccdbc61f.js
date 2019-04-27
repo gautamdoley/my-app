4ee2098af18c738df7af7756ce5923abc2184f54
gautamdoley <gautam.dole@gmail.com>
2019-04-27 23:30:37 +0530

    Updates

---
+++ precache-manifest.e830fd0d66d43b0ebfccd621ccdbc61f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "100003ba136d6db0bba3",
+    "url": "/gautamdoley/my-app/static/js/runtime~main.bc7fe720.js"
+  },
+  {
+    "revision": "a1ad55f0ca6ecba2ad33",
+    "url": "/gautamdoley/my-app/static/js/main.3aef4a79.chunk.js"
+  },
+  {
+    "revision": "2315f74ce3c99316133d",
+    "url": "/gautamdoley/my-app/static/js/2.3310f33a.chunk.js"
+  },
+  {
+    "revision": "a1ad55f0ca6ecba2ad33",
+    "url": "/gautamdoley/my-app/static/css/main.8bd2eb0a.chunk.css"
+  },
+  {
+    "revision": "f6b6ffc4374beb448ec43782c5ad1d17",
+    "url": "/gautamdoley/my-app/index.html"
+  }
+];
